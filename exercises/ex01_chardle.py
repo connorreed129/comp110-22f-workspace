d16d6c1b7fac0e9ae10ac19775f87a6cda61f59a
Connor Reed <creed2@unc.edu>
2022-08-28 22:53:22 -0400

    FINAL EX01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,13 +2,18 @@
 __author__ = "730478163"
 
 word_guess: str = input("Enter a five character word: ")
+if len(str(word_guess)) != 5:
+    print("Error: Word must contain 5 characters ")
+    exit()
 character_guess: str = input("Enter a single character: ")
+if len(str(character_guess)) != 1:
+    print("Error: Character must be a single character. ")
+    exit()
 print("Searching for " + character_guess + " in " + word_guess)
-
 if character_guess == word_guess[0]:
     print(character_guess + " found at index 0 ")
 if character_guess == word_guess[1]:
-    print(character_guess + " found at index 1 " )
+    print(character_guess + " found at index 1 ")
 if character_guess == word_guess[2]:
     print(character_guess + " found at index 2 ")
 if character_guess == word_guess[3]:
@@ -19,15 +24,15 @@ if character_guess == word_guess[4]:
 instances_counter: int = 0
 
 if character_guess == word_guess[0]:
-    instances_counter: int = instances_counter + 1
+    instances_counter = int = instances_counter + 1
 if character_guess == word_guess[1]:
-    instances_counter: int = instances_counter + 1
+    instances_counter = int = instances_counter + 1
 if character_guess == word_guess[2]:
-    instances_counter: int = instances_counter + 1
+    instances_counter = int = instances_counter + 1
 if character_guess == word_guess[3]:
-    instances_counter: int = instances_counter + 1
+    instances_counter = int = instances_counter + 1
 if character_guess == word_guess[4]:
-    instances_counter: int = instances_counter + 1
+    instances_counter = int = instances_counter + 1
 
 if instances_counter > 1:
     print(str(instances_counter) + " instances of " + str(character_guess) + " found in " + str(word_guess))
@@ -35,9 +40,3 @@ if instances_counter == 1:
     print(str(instances_counter) + " instance of " + str(character_guess) + " found in " + str(word_guess))
 if instances_counter < 1:
     print("No instances of " + str(character_guess) + " found in " + str(word_guess))
-if len(word_guess) != 5:
-    print("Error word must contain 5 letters. ")
-    exit() 
-if len(character_guess) != 1:
-    print("Error: Character must be a single character. ")
-    exit()
