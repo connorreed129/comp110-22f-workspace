1d6437f426bbe4f3cbf10f8bef7878494ad9b46c
Connor Reed <creed2@unc.edu>
2022-09-04 13:07:48 -0400

    ex02 progress

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -5,12 +5,12 @@ __author__: "730478163"
 SECRET: str = "python"
 secret_letter_counter: len(SECRET)
 
-user_guess: input("What is your " + str(secret_letter_counter) + "-letter guess? ")
+user_guess: input(f"What is your {secret_letter_counter}-letter guess? ")
 
 while len(user_guess) != secret_letter_counter:
-    print(input("That was not " + str(secret_letter_counter) + " letters! Try again: "))
+    print(input(f"That was not {int(secret_letter_counter)} letters! Try again: "))
 
 if user_guess == SECRET:
     print("Woo! You got it! ")
 else:
-    print("Not quite. Play again soon! ")
+    print("Not quite. Play again soon! ") 
