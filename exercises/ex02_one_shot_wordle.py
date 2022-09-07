a4ced24b8ee4adbebb0e7f1202c6d616ff8d1e5b
Connor Reed <creed2@unc.edu>
2022-09-07 15:10:29 -0400

    final ex02_one_shot_wordle

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,41 +1,43 @@
-"""One Shot Wordle"""
-
-__author__: "730478163"
+"""One Shot Wordle."""
+ 
+__author__ = "730478163"
 SECRET: str = "python"
 secret_letter_counter: int = len(SECRET)
-
+ 
 user_guess: str = input(f"What is your {int(secret_letter_counter)}-letter guess? ")
-
+ 
 while len(user_guess) != secret_letter_counter:
-    user_guess: str = input(f"That was not {int(secret_letter_counter)} letters! Try again: ")
-
-
+    user_guess = input(f"That was not {int(secret_letter_counter)} letters! Try again: ")
+ 
+ 
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-character_existence: bool = False 
+emoji: str = ""
+"""emoji += box instead of print emojis """
+character_existence: bool = False
 alternate_index_tracker: int = 0
-
-
+ 
+ 
 index_tracker: int = 0
 while index_tracker < len(SECRET):
     if user_guess[index_tracker] == SECRET[index_tracker]:
-        print(GREEN_BOX, end = '')
-    else: 
+        emoji += GREEN_BOX
+    else:
         character_existence = False
         alternate_index_tracker = 0
-        while character_existence == False and alternate_index_tracker < len(SECRET):
+        while character_existence is False and alternate_index_tracker < len(SECRET):
             if user_guess[index_tracker] == SECRET[alternate_index_tracker]:
                 character_existence = True
             else:
-                alternate_index_tracker += 1 
-        if character_existence == True:
-            print(YELLOW_BOX, end = '')
+                alternate_index_tracker += 1
+        if character_existence is True:
+            emoji += YELLOW_BOX
         else:
-            print(WHITE_BOX, end = '')
+            emoji += WHITE_BOX
     index_tracker += 1
-
+print(emoji)
 if user_guess == SECRET:
-    print("\nWoo! You got it! ")
+    print("Woo! You got it! ")
 else:
-    print("\nNot quite. Play again soon! ")
+    print("Not quite. Play again soon! ")
